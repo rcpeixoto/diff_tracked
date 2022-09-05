965ae6bb8e61c45a043f7ce3277f51f142159425
rcpeixoto <raul.p.carmo@gmail.com>
2022-09-05 09:31:00 -0300

    Ajust i2c

--- scripts/i2c_comns.py
+++ scripts/i2c_comns.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import Range
 from sensor_msgs.msg import Imu
 from std_msgs.msg import String
 from std_msgs.msg import Int16
-from std_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3
 bus = smbus.SMBus(1)
 
 
@@ -23,7 +23,6 @@ def talker():
 
     wheel_speed = rospy.Subscriber("cmd_msg", String, callback)
 
-    imu = rospy.Publisher("imu_raw_data", Imu, queue_size=10)
     lwheel = rospy.Publisher("lwheel", Int16, queue_size=10)
     rwheel = rospy.Publisher("rwheel", Int16, queue_size=10)
     sonar = rospy.Publisher("sonar", Range, queue_size=10)
@@ -32,17 +31,9 @@ def talker():
     
     while not rospy.is_shutdown():
 
-        sonar_data = ((bus.read_byte(0x12) << 8) & 0xff00) +  bus.read_byte(0x12)
-
-        AcX = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        AcY = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        AcZ = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        GyX = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        GyY = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        GyZ = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-
-        leftCount = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
-        rightCount = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        sonar_data = (bus.read_byte(0x12) << 8) +  bus.read_byte(0x12)
+        leftCount = (bus.read_byte(0x12) << 8) + bus.read_byte(0x12)
+        rightCount = (bus.read_byte(0x12) << 8) + bus.read_byte(0x12)
         
         msg = Int16()
         msg.data = leftCount
@@ -62,24 +53,6 @@ def talker():
         msg.range = sonar_data/100
 
         sonar.publish(msg)
-
-        accel = Vector3()
-        accel.x = AcY
-        accel.y = AcY
-        accel.z = AcZ
-
-        gyro = Vector3()
-        gyro.x = GyX
-        gyro.y = GyY
-        gyro.z = GyZ
-
-        imu_data = Imu()
-        imu_data.linear_acceleration = accel
-        imu_data.angular_velocity = gyro
-        imu_data.header.stamp = rospy.Time.now()
-        imu_data.header.frame_id = "mpu_link"
-        
-        imu.publish(imu_data)
 
         rospy.spin()
         r.sleep()
