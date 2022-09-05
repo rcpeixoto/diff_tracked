37e8e62be22e76142a246ce27fd47b3a18564965
Ginnungagap <raul.p.carmo@gmail.com>
2022-09-04 21:09:26 -0300

    update i2c communication

--- scripts/i2c_comns.py
+++ scripts/i2c_comns.py
@@ -3,7 +3,7 @@
 import smbus
 import rospy
 
-from sensor_msgs.msg import Range
+from sensor_msgs.msg import Range 
 from sensor_msgs.msg import Imu
 from std_msgs.msg import String
 from std_msgs.msg import Int16
@@ -12,11 +12,17 @@ bus = smbus.SMBus(1)
 
 
 def callback(data):
-    bus.write_i2c_block_data(0x08, 0x00, data.data)
+    cmd = data.data
+    count = 0
+    while count < len(cmd):
+        bus.write_byte(0x12, cmd[count])
+        count = count + 1
 
 def talker():
     rospy.init_node("i2c_comns", anonymous=True)
+
     wheel_speed = rospy.Subscriber("cmd_msg", String, callback)
+
     imu = rospy.Publisher("imu_raw_data", Imu, queue_size=10)
     lwheel = rospy.Publisher("lwheel", Int16, queue_size=10)
     rwheel = rospy.Publisher("rwheel", Int16, queue_size=10)
@@ -26,14 +32,57 @@ def talker():
     
     while not rospy.is_shutdown():
 
+        sonar_data = ((bus.read_byte(0x12) << 8) & 0xff00) +  bus.read_byte(0x12)
+
+        AcX = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        AcY = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        AcZ = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        GyX = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        GyY = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        GyZ = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+
+        leftCount = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
+        rightCount = ((bus.read_byte(0x12) << 8) & 0xff00) | bus.read_byte(0x12)
         
+        msg = Int16()
+        msg.data = leftCount
+        lwheel.publish(msg)
+
+        msg = Int16()
+        msg.data = rightCount
+        rwheel.publish(msg)
+
+        msg = Range()
+        msg.header.frame_id = "sonar_link"
+        msg.header.stamp = rospy.Time.now()
+        msg.radiation_type = Range.ULTRASOUND
+        msg.field_of_view = 0.523559
+        msg.min_range = 0.02
+        msg.max_range = 4
+        msg.range = sonar_data/100
+
+        sonar.publish(msg)
+
+        accel = Vector3()
+        accel.x = AcY
+        accel.y = AcY
+        accel.z = AcZ
+
+        gyro = Vector3()
+        gyro.x = GyX
+        gyro.y = GyY
+        gyro.z = GyZ
+
+        imu_data = Imu()
+        imu_data.linear_acceleration = accel
+        imu_data.angular_velocity = gyro
+        imu_data.header.stamp = rospy.Time.now()
+        imu_data.header.frame_id = "mpu_link"
+        
+        imu.publish(imu_data)
 
         rospy.spin()
         r.sleep()
-
-
-
-
 
 if __name__ == '__main__':
     try:
