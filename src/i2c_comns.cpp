db2e04098675edd66a832f8b325f5e115078907e
Ginnungagap <raul.p.carmo@gmail.com>
2022-09-12 13:06:18 -0300

    C++ module for i2c

---
+++ src/i2c_comns.cpp
@@ -0,0 +1,82 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/Int16.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/Vector3.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+#define PIC_ADDR 0x12
+#define IMU_ADDR 0x68
+
+
+void callback(const std_msgs::Strang::ConstPtr& msg){
+    
+}
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "i2c_comns");
+    ros::NodeHandle nh;
+    ros::Subscriber cmd_vel = nh.subscribe<std_msgs::String>("cmd_msg", 10, callback);
+
+    ros::Publisher sonar = nh.advertise<sensor_msgs::Range>("sonar", 10);
+    ros::Publisher left_wheel = nh.advertise<std_msgs::Int16>("lwheel", 10);
+    ros::Publisher right_wheel = nh.advertise<std_msgs::Int16>("rwheel", 10);
+    ros::Publisher imu_data = nh.advertise<sensor_msgs::Imu>("imu", 10);
+
+    ros::Rate r(4);
+
+    int file;
+    char *filename = "/dec/i2c-1";
+
+    if((file = open(filename, O_RDWR)) < 0) {
+        exit(1);
+    }
+
+
+    while(nh.ok()){
+        //Reads data from Pic
+        if(ioctl(file, I2C_SLAVE, PIC_ADDR) < 0){
+            exit(1);
+        }   
+        char buf[6];
+        read(file, buf, 6);
+        int sonar_data = (int) (buf[0] << 8) | buf[1];
+        std_msgs::Int16 left_count;
+        left_count.data = (int) (buf[2] << 8) | buf[3];
+        std_msgs::Int16 right_count;
+        right_count.data = (int) (buf[4] << 8) | buf[5];    
+        //Publishes data from pic
+        sensor_msgs::Range msg;
+        msg.header.frame_id = "sonar_link";
+        msg.header.stamp = nh.now();
+        msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+        msg.field_of_view = 0.523559;
+        msg.min_range = 0.02;
+        msg.max_range = 4;
+        msg.range = sonar_data/100;
+
+        sonar.publish(&msg);
+        left_wheel.publish(&left_count);
+        rightWheel.publish(&right_count);
+
+        //Reads from IMU
+
+
+        ros::spinOnce();
+        r.sleep();
+    }
+    return 0;
+}
--- src/wheel_velocity_controller.cpp
+++ src/wheel_velocity_controller.cpp
@@ -8,29 +8,7 @@
 
 double vel = 0;
 double omega = 0;
-/*
-void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
-    double vel = msg->linear.x;
-    double omega = msg->angular.z;
-    double rightWheel = (vel + omega*(WHEEL_DISTANCE/2));
-    double leftWheel  = (vel - omega*(WHEEL_DISTANCE/2));
 
-    int left_wheel_cmd = -2699910*pow(leftWheel, 6) + 3045080*pow(leftWheel, 5) + 115204*pow(leftWheel, 4) - 180807*pow(leftWheel, 3) - 1179.32*pow(leftWheel, 2) + 4485.28*leftWheel + 1500;
-    int right_wheel_cmd = -2699910*pow(rightWheel, 6) + 3045080*pow(rightWheel, 5) + 115204*pow(rightWheel, 4) - 180807*pow(rightWheel, 3) - 1179.32*pow(rightWheel, 2) + 4485.28*rightWheel + 1500;
-
-    int rightFoward = (right_wheel_cmd > 1500? 1 : 0);
-    int leftFoward = (left_wheel_cmd > 1500? 1 : 0);
-
-    std::string cmd = "#12P" + std::to_string(left_wheel_cmd) +"T1000#13P" + 
-        std::to_string(right_wheel_cmd) + "T1000\r" + ";"+ std::to_string(rightFoward) + ";" + std::to_string(leftFoward);
-
-    std_msgs::String cmd_send;
-    cmd_send.data = cmd;
-
-    pub_cmd_vel.publish(cmd_send);
-
-}
-*/
 void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
     vel = msg->linear.x;
     omega = msg->angular.z;
@@ -39,9 +17,9 @@ void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
 int main(int argc, char** argv){
     ros::init(argc, argv, "wheel_velocity_controller");
     ros::NodeHandle n;
-    ros::Publisher pub_cmd_vel = n.advertise<std_msgs::String>("cmd_msg", 100000000);
-    ros::Subscriber sub_cmd_vel = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10000000, cmd_vel_callback);
-    ros::Rate r(5);
+    ros::Publisher pub_cmd_vel = n.advertise<std_msgs::String>("cmd_msg", 10);
+    ros::Subscriber sub_cmd_vel = n.subscribe<geometry_msgs::Twist>("cmd_vel", 10, cmd_vel_callback);
+    ros::Rate r(4);
 
     while(n.ok()){
         double rightWheel = (vel + omega*(WHEEL_DISTANCE/2));
