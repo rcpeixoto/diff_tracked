6ee248bb85a281445818281d6c515b2cde487b9c
rcpeixoto <raul.p.carmo@gmail.com>
2022-09-18 08:56:48 -0300

    i2c communication fixed

--- src/i2c_comns.cpp
+++ src/i2c_comns.cpp
@@ -4,9 +4,13 @@
 #include <std_msgs/Int16.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Vector3.h>
-#include <linux/i2c-dev.h>
+
+extern "C"{
+	#include <linux/i2c-dev.h>
+	#include <i2c/smbus.h>
+}
+
 #include <errno.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -40,87 +44,31 @@ int main(int argc, char** argv){
     geometry_msgs::Vector3 accel;
     geometry_msgs::Vector3 gyro;
     sensor_msgs::Imu imu;
-
+    
+    //Opens i2c bus
     int file;
     char filename[20];
-    char buf[1];
-    char addr[1];
-
     snprintf(filename, 20, "/dev/i2c-%d", 1);
-
     if((file = open(filename, O_RDWR)) < 0) exit(1);
 
+    //Sets IMU as slave
     if(ioctl(file, I2C_SLAVE, IMU_ADDR) < 0) exit(1);
-    
 
-    //Configure IMU
-    addr[0] = 0x6b;
-    buf[0] = 0x00;
-    write(file, addr, 1);
-    write(file, buf, 1);
+    i2c_smbus_write_byte_data(file, 0x6b, 0x00);
+    i2c_smbus_write_byte_data(file, 0x1b, 0x00);
+    i2c_smbus_write_byte_data(file, 0x1c, 0x00);
+    i2c_smbus_write_byte_data(file, 0x37, 0x90);
+    i2c_smbus_write_byte_data(file, 0x38, 0x01);
 
-    addr[0] = 0x1b;
-    buf[0] = 0x00;
-    write(file, addr, 1);
-    write(file, buf, 1);
 
-    addr[0] = 0x1c;
-    buf[0] = 0x00;
-    write(file, addr, 1);
-    write(file, buf, 1);
-
-    addr[0] = 0x37;
-    buf[0] = 0x90;
-    write(file, addr, 1);
-    write(file, buf, 1);
-
-    addr[0] = 0x38;
-    buf[0] = 0x01;
-    write(file, addr, 1);
-    write(file, buf, 1);
-    close(file);
 
     while(nh.ok()){
+	if(ioctl(file, I2C_SLAVE, IMU_ADDR) < 0) exit(1);
+        __u8 data[14];
+        __u8 reg = 0x3b;
+	__u8 length = 14;
+        i2c_smbus_read_i2c_block_data(file, reg, length, data);
 
-        //Reads data from Pic
-	if((file = open(filename, O_RDWR)) < 0) exit(1);
-
-	if(ioctl(file, I2C_SLAVE, PIC_ADDR) < 0) exit(1);
-
-        char buf[6];
-        read(file, buf, 6);
-        int sonar_data = (int) (buf[0] << 8) | buf[1];
-        std_msgs::Int16 left_count;
-        left_count.data = (int) (buf[2] << 8) | buf[3];
-        std_msgs::Int16 right_count;
-        right_count.data = (int) (buf[4] << 8) | buf[5];    
-        //Publishes data from pic
-        sensor_msgs::Range msg;
-        msg.header.frame_id = "sonar_link";
-        msg.header.stamp = ros::Time::now();
-        msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-        msg.field_of_view = 0.523559;
-        msg.min_range = 0.02;
-        msg.max_range = 4;
-        msg.range = sonar_data/100;
-
-        sonar.publish(msg);
-        left_wheel.publish(left_count);
-        right_wheel.publish(right_count);
-
-	close(file);
-
-	if((file = open(filename, O_RDWR)) < 0) exit(1);
-
-	if(ioctl(file, I2C_SLAVE, IMU_ADDR)) exit(1);
-
-        //Reads from IMU
-        char data[14];
-	addr[0] = 0x3b;
-
-        write(file, addr, 1);
-        read(file, data, 14);
-        
         accel.x = ((short int) (data[0] << 8) | data[1])/16384.0;
         accel.y = ((short int) (data[2] << 8) | data[3])/16384.0;
         accel.z = ((short int) (data[4] << 8) | data[5])/16384.0;
@@ -134,11 +82,32 @@ int main(int argc, char** argv){
 	imu.header.frame_id = "mpu_link";
 	imu.header.stamp = ros::Time::now();        
 
+        if(ioctl(file, I2C_SLAVE, PIC_ADDR) < 0) exit(1);
+        
+        int sonar_data = (int) (i2c_smbus_read_byte(file) << 8) | i2c_smbus_read_byte(file);
+        std_msgs::Int16 left_count;
+        left_count.data = (int) (i2c_smbus_read_byte(file) << 8) | i2c_smbus_read_byte(file);
+        std_msgs::Int16 right_count;
+        right_count.data = (int) (i2c_smbus_read_byte(file) << 8) | i2c_smbus_read_byte(file);    
+        //Publishes data from pic
+        sensor_msgs::Range msg;
+        msg.header.frame_id = "sonar_link";
+        msg.header.stamp = ros::Time::now();
+        msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+        msg.field_of_view = 0.523559;
+        msg.min_range = 0.02;
+        msg.max_range = 4;
+        msg.range = sonar_data/100.0;
+
+
+        sonar.publish(msg);
+        left_wheel.publish(left_count);
+        right_wheel.publish(right_count);
         imu_data.publish(imu);
-        close(file);
         ros::spinOnce();
         r.sleep();
     }
     close(file);
     return 0;
 }
+
